f0ed7c41d98953c18d15b59ac820fcae91fd462c
sgcs11 <joachy@naver.com>
2022-08-01 11:49:53 +0900

    8월 1일 수업 시작

---
+++ src/main/java/com/ll/exam/chat/ChatController.java
@@ -0,0 +1,226 @@
+package com.ll.exam.chat;
+
+import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.chat.dto.ChatMessageDto;
+import com.ll.exam.chat.dto.ChatRoomDto;
+
+import java.util.List;
+
+public class ChatController {
+    private ChatService chatService;
+
+    public ChatController() {
+        chatService = new ChatService();
+    }
+
+    public void showCreateRoom(Rq rq) {
+        rq.view("usr/chat/createRoom");
+    }
+
+    public void doCreateRoom(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        long id = chatService.createRoom(title, body);
+
+        rq.replace("/usr/chat/room/%d".formatted(id), "%d번 채팅방이 생성 되었습니다.".formatted(id));
+    }
+
+    public void showRoomList(Rq rq) {
+        List<ChatRoomDto> chatRoomDtos = chatService.findAllRooms();
+
+        rq.setAttr("rooms", chatRoomDtos);
+        rq.view("usr/chat/roomList");
+    }
+
+    public void showModifyRoom(Rq rq) {
+        long id = rq.getLongPathValueByIndex(0, -1);
+
+        if (id == -1) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoom = chatService.findRoomById(id);
+
+        if (chatRoom == null) {
+            rq.historyBack("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        rq.setAttr("room", chatRoom);
+        rq.view("usr/chat/modifyRoom");
+    }
+
+    public void doModifyRoom(Rq rq) {
+        long id = rq.getLongPathValueByIndex(0, -1);
+
+        if (id == -1) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        String title = rq.getParam("title", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        String body = rq.getParam("body", "");
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoom = chatService.findRoomById(id);
+
+        if (chatRoom == null) {
+            rq.historyBack("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        chatService.modifyRoom(id, title, body);
+
+        rq.replace("/usr/chat/room/%d".formatted(id), "%d번 채팅방이 수정되었습니다.".formatted(id));
+    }
+
+    public void deleteRoom(Rq rq) {
+        long id = rq.getLongPathValueByIndex(0, 0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoomDto = chatService.findRoomById(id);
+
+        if (chatRoomDto == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        chatService.deleteRoom(id);
+
+        rq.replace("/usr/chat/roomList", "%d번 채팅방이 삭제되었습니다.".formatted(id));
+    }
+
+    // ajax 방식으로 채팅메세지를 리스팅
+    public void showRoom(Rq rq) {
+        long id = rq.getLongPathValueByIndex(0, -1);
+
+        if (id == -1) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoomDto = chatService.findRoomById(id);
+
+        if (chatRoomDto == null) {
+            rq.historyBack("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        rq.setAttr("room", chatRoomDto);
+
+        rq.view("usr/chat/room");
+    }
+
+    public void showRoomManual(Rq rq) {
+        long id = rq.getLongPathValueByIndex(0, -1);
+
+        if (id == -1) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoomDto = chatService.findRoomById(id);
+        List<ChatMessageDto> chatMessageDtos = chatService.findMessagesByRoomId(chatRoomDto.getId());
+
+        if (chatRoomDto == null) {
+            rq.historyBack("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        rq.setAttr("room", chatRoomDto);
+        rq.setAttr("messages", chatMessageDtos);
+
+        rq.view("usr/chat/roomManual");
+    }
+
+    public void doWriteMessage(Rq rq) {
+        long roomId = rq.getLongPathValueByIndex(0, -1);
+
+        if (roomId == -1) {
+            rq.historyBack("채팅방 번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoom = chatService.findRoomById(roomId);
+
+        if (chatRoom == null) {
+            rq.historyBack("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        String body = rq.getParam("body", "");
+
+        if (body.trim().length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        chatService.writeMessage(roomId, body);
+
+        rq.replace("/usr/chat/room/%d".formatted(roomId), "메세지가 등록되었습니다.");
+    }
+
+    public void getMessages(Rq rq) {
+        long roomId = rq.getLongPathValueByIndex(0, -1);
+
+        if (roomId == -1) {
+            rq.failJson("채팅방 번호를 입력해주세요.");
+            return;
+        }
+
+        ChatRoomDto chatRoom = chatService.findRoomById(roomId);
+
+        if (chatRoom == null) {
+            rq.failJson("존재하지 않는 채팅방 입니다.");
+            return;
+        }
+
+        long fromId = rq.getLongParam("fromId", -1);
+
+        List<ChatMessageDto> chatMessageDtos = null;
+
+        if ( fromId == -1 ) {
+            chatMessageDtos = chatService.findMessagesByRoomId(roomId);
+        }
+        else {
+            chatMessageDtos = chatService.findMessagesByRoomIdGreaterThan(roomId, fromId);
+        }
+
+        rq.successJson(chatMessageDtos);
+    }
+
+    public void doWriteMessageAjax(Rq rq) {
+        // roomId 구하고
+        // body 구하고
+        // 등록
+
+        rq.successJson(null);
+    }
+}
---
+++ src/main/webapp/jsp/usr/chat/room.jsp
@@ -0,0 +1,96 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<%@ include file="../common/head.jspf"%>
+
+<script>
+function ChatMessageSave__submitForm(form) {
+    form.body.value = form.body.value.trim();
+
+    if ( form.body.value.length == 0 ) {
+        form.body.focus();
+
+        return false;
+    }
+
+    // fetch 방식이 아닌, jquery 방식
+    $.post(
+        '/usr/chat/writeMessageAjax/${room.id}' // 주소, action
+        {
+            body: form.body.value // 폼 내용, input name, value
+        },
+        function(data) { // 콜백 메서드, 통신이 완료된 후, 실행
+            // data.resultCode
+            // data.msg
+        },
+        'json' // 받은 데이터를 json 으로 해석하겠다.
+    );
+
+    form.body.value = '';
+    form.body.focus();
+}
+</script>
+
+
+<script>
+let ChatMessages__lastId = 0;
+function ChatMessages__loadMore() {
+    fetch(`/usr/chat/getMessages/${room.id}/?fromId=\${ChatMessages__lastId}`)
+        .then(data => data.json())
+        .then(responseData => {
+            const messages = responseData.data;
+            for ( const index in messages ) {
+                const message = messages[index];
+
+                const html = `
+                    <li class="flex">
+                        <span>메세지 \${message.id} :</span>
+                        &nbsp;
+                        <span>\${message.body}</a>
+                        <a onclick="if ( !confirm('정말로 삭제하시겠습니까?') ) return false;" class="hover:underline hover:text-[red] mr-2" href="/usr/chat/deleteMessage/\${message.id}?_method=DELETE">삭제</a>
+                    </li>
+                `;
+
+                $('.chat-messages').append(html);
+            }
+
+            if ( messages.length > 0 ) {
+                ChatMessages__lastId = messages[messages.length - 1].id;
+            }
+
+            // ChatMessages__loadMore(); // 즉시 실행
+            setTimeout(ChatMessages__loadMore, 3000); // ChatMessages__loadMore(); 를 3초 뒤에 수행
+        });
+}
+</script>
+
+<section>
+    <div class="container px-3 mx-auto">
+        <h1 class="font-bold text-lg">채팅방</h1>
+
+        <div>
+            ${room.title}
+        </div>
+
+        <div>
+            ${room.body}
+        </div>
+
+        <form onsubmit="ChatMessageSave__submitForm(this); return false;" method="POST" action="/usr/chat/writeMessage/${room.id}">
+            <input autofocus name="body" type="text" placeholder="메세지를 입력해주세요." class="input input-bordered" />
+            <button type="submit" value="" class="btn btn-outline btn-primary">
+                작성
+            </button>
+        </form>
+
+        <ul class="chat-messages mt-5">
+
+        </ul>
+    </div>
+</section>
+
+<script>
+ChatMessages__loadMore();
+</script>
+
+<%@ include file="../common/foot.jspf"%>
